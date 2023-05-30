7fe1d8b5988b936621095ed06d55b065ec839a35
João Victor Cordeiro <joao.cordeiro@liferay.com>
2023-05-30 17:21:30 -0300

    LPS-186244 URL object was being overwritten and breaking the fetch

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/fetch.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/fetch.es.js
@@ -38,7 +38,7 @@ export default function defaultFetch(resource, init = {}) {
 				resource = resourceLocation;
 			}
 			else {
-				resource = {...resource, url: resourceLocation};
+				resource = new URL(resourceLocation);
 			}
 		}
 
@@ -64,7 +64,7 @@ export default function defaultFetch(resource, init = {}) {
 				resource = resourceLocation;
 			}
 			else {
-				resource = {...resource, url: resourceLocation};
+				resource = new URL(resourceLocation);
 			}
 		}
 	}
